7ba05fff1dda6927a7edcad2de5b5e5a8ebac28f
uxharshit <harshitkatheria7890@gmail.com>
2024-05-31 09:29:15 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
     <BrowserRouter>
     <Routes>
-      <Route path='/' element={<Home/>}/>
+      <Route path='/HexClan' element={<Home/>}/>
       <Route path='*' element={<h1>Not Found 404</h1>}/>
     </Routes>
     </BrowserRouter>
